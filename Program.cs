b9289ca4ad709ee02ddc26cbc49e80acdcca1119
nua <enuesaa@gmail.com>
2026-08-09 16:06:03 +0900

    chore: playgroundで作ったプログラムをコピーしてきた

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Sharprompt;
+
+class Program
+{
+    static void Main()
+    {
+        var files = Directory.GetFiles(".");
+
+        if (files.Length == 0)
+        {
+            Console.Error.WriteLine("There are no files here.");
+            Environment.Exit(1);
+        }
+
+        while (true)
+        {
+            string selected = Prompt.Select("Please select a file", items: files);
+            // remove prompt
+            int line = Console.CursorTop;
+            Console.SetCursorPosition(0, line - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, line - 1);
+            // stdout file
+            FileViewer.Show(selected);
+        }
+    }
+}
